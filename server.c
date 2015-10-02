cd0bd653945f5cfedfefa820f683ed67f103b307
Masukami <suhailqamil@gmail.com>
2015-10-02 08:55:32 +0800

    Added error checking for socket,bind, and listen

--- server.c
+++ server.c
@@ -1,17 +1,13 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <time.h>
+#include <sys/types.h> //for socket
+#include <sys/socket.h>
+#include <netinet/in.h> //for struct
+#include <errno.h> //for error
 
 int main(int argc, char * argv[])
 {
-	int socket;
+	int socket,len;
 	struct sockaddr_in server;
 		
 	
@@ -23,5 +19,22 @@ int main(int argc, char * argv[])
 	} 
 	//af_inet - domain, sock_stream - tcp, 0 - protocol
         // -1 = error
-	
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(1036);
+	server.sin_addr.s_addr = INADDR_ANY;
+	bzero(&server.sin_zero, 8);
+
+	len = sizeof(struct sockaddr_in);
+	// error checking for bind
+	if((bind (sock, (struct sockaddr *)&server, len)) == -1)
+	{
+		perror("bind");
+		exit(-1);
+	}	
+	if((listen (sock, 5)) == -1)
+	{
+		perror("listen");
+		exit(-1);
+	}
 }

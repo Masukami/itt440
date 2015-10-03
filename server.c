e99f6ed4d09781842845133c5fa7a683e1c16eb5
Masukami <suhailqamil@gmail.com>
2015-10-03 08:21:38 +0800

    Completed Server coding that can listen with socket port of 1036

--- server.c
+++ server.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h> //for socket
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h> //for struct
-#include <errno.h> //for error
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc, char * argv[])
+void error(char *msg)
 {
-	int socket,len;
-	struct sockaddr_in server;
-		
-	
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		//find out what's the error
-		perror("socket : ");
-		exit(-1);
-	} 
-	//af_inet - domain, sock_stream - tcp, 0 - protocol
-        // -1 = error
+	perror(msg);
+	exit(1);
+}
 
+int main(int argc, char *argv[])
+{
+	int sock, cli, sent;
+	unsigned int len;
+	struct sockaddr_in server, client;
+	sock = socket(AF_INET, SOCK_STREAM, 0); //initialize socket
+	if( sock < 0 )
+		error("socket : ");
+		
 	server.sin_family = AF_INET;
 	server.sin_port = htons(1036);
 	server.sin_addr.s_addr = INADDR_ANY;
 	bzero(&server.sin_zero, 8);
-
+	
 	len = sizeof(struct sockaddr_in);
-	// error checking for bind
-	if((bind (sock, (struct sockaddr *)&server, len)) == -1)
+	
+	if((bind(sock, (struct sockaddr *)&server, len)) < 0 )
+		error("bind");
+	if( (listen(sock, 5)) < 0 )
+		error("listen");
+	
+	while(1)
 	{
-		perror("bind");
-		exit(-1);
-	}	
-	if((listen (sock, 5)) == -1)
-	{
-		perror("listen");
-		exit(-1);
+		if( (cli = accept(sock, (struct sockaddr *)&client, &len)) < 0)
+			error("accept");
 	}
+	
 }

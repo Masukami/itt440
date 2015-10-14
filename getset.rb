2b3293dd630065224dff41edbe54f42326a9f243
Masukami <suhailqamil@gmail.com>
2015-10-14 15:07:18 +0800

    Socket Option Edited and Result are displayed

--- getset.rb
+++ getset.rb
@@ -6,8 +6,11 @@ socket = TCPServer.new('0.0.0.0', PORT)
 #getsockopt(level,optname)
 #setsockopt(level,optname,val)
 
-optval = socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_SNDLOWAT);
-puts "SNDBUF Early Option : #{Socket::SO_SNDLOWAT}"
-socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_SNDLOWAT, 5342);
-puts "SNDBUF After Edit : #{Socket::SO_SNDLOWAT}"
-
+optval = socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF);
+optval = optval.unpack "i"
+puts "SNDBUF Early Option : #{optval}"
+socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF, 5342);
+optval = socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF);
+optval = optval.unpack "i"
+puts "SNDBUF After Edit : #{optval}"
+	
